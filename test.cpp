2a434429c488d6cc51a865ae355389fc136c4f64
MikFuentes <33886316+MikFuentes@users.noreply.github.com>
2019-04-15 19:40:18 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,9 +1,35 @@
-#include <cstdio>
 #include <iostream>
-#include <array>
+#include <vector>
+#include <deque>
 using namespace std;
 
-
+void printEverything(vector<int> availableResources, deque<vector<int>> currentProcesses, deque<vector<int>> goalProcesses){
+	string output = "\n";
+	
+	//Available Resources
+	output+= "Available:\n";
+	for(int aResourecs : availableResources){
+		output += to_string(aResourecs) + " ";
+	} output += "\n\n";
+	
+	//Current Processes
+	output+= "Current Processes:\n";
+	for(vector<int> v : currentProcesses) {
+		for(int elem : v){
+			output += to_string(elem) + " ";
+		} output += "\n";
+	}output += "\n";
+	
+	//Final Processes
+	output+= "Process Goals:\n";
+	for(vector<int> v : goalProcesses) {
+		for(int elem : v){
+			output += to_string(elem) + " ";
+		} output += "\n";
+	}
+	
+	cout << output << endl;
+}
 
 int main(){
 	int numCases, num;
@@ -12,60 +38,51 @@ int main(){
 	//For each test case
 	for(int i=0; i<numCases; i++){
 		
-		//*GET INPUT*//
+		//*INPUT*//
 		//Get inputs for the number of processes and number of resources types
 		int numProcesses, numResourceTypes;
 		cin >> numProcesses >> numResourceTypes;
 		
-		//Make new array for the resources available
-		int availableResources [numResourceTypes]; //instantiate an empty array matching size of numResourceTypes
-		//array<int, numResourceTypes> availableResources;
+		//Make new vector for the resources available
+		vector<int> availableResources(numResourceTypes); //instantiate an empty vector matching size of numResourceTypes
 		
-		//Make new array for the current processes
-		int currentProcesses [numResourceTypes][numProcesses]; //instantiate an empty 2d array matching the size of numResourceTypes and numProcesses
-		//array<int, numResourceTypes, numProcesses> currentProcesses;
+		//Make a matrix for the current processes
+		deque<vector<int>> currentProcesses;
 		
-		//Make new array to compare
-		int finalProcesses [numResourceTypes][numProcesses]; //instantiate an empty 2d array matching the size of numResourceTypes and numProcesses
-		//array<int, numResourceTypes, numProcesses> finalProcesses;
+		//Make matrix to compare
+		deque<vector<int>> goalProcesses;
 		
-		//Input initial number of each available resource
-		for(int j=0; j<numResourceTypes;j++){
-			cin >> num;
-			availableResources[j] = num;  
-		}
+		//Input initial number of each available resources
+		for(int j=0; j<numResourceTypes;j++) cin >> availableResources[j];  
 		
 		//Input initial number of each resource that each process is holding
 		for(int j=0;j<numProcesses; j++){
+			vector<int> inter_mat;  //Intermediate matrix to help insert(push) contents of whole row at a time
 			for (int k=0; k<numResourceTypes; k++) {
 				cin >> num;
-				currentProcesses[k][j] = num;  
-			}
+				inter_mat.push_back(num);
+			} 
+			currentProcesses.push_back(inter_mat);
 		}
 		
 		//Input initial number of each resource that each process is supposed to hold to run
 		for(int j=0;j<numProcesses; j++){
+			vector<int> inter_mat;  //Intermediate matrix to help insert(push) contents of whole row at a time
 			for (int k=0; k<numResourceTypes; k++) {
 				cin >> num;
-				finalProcesses[k][j] = num;  
-			}
+				inter_mat.push_back(num);
+			} 
+			goalProcesses.push_back(inter_mat);
 		}
 		
 		//*CHECK PROCESSES*//
-		cout << endl;
-		for(int j=0;j<numProcesses; j++){
-			for (int k=0; k<numResourceTypes; k++) {
-				cout << currentProcesses[k][j] << " "; 
-			}cout << endl;
-		}
+		printEverything(availableResources, currentProcesses, goalProcesses);
 		
-		cout << endl;
-		for(int j=0;j<numProcesses; j++){
-			for (int k=0; k<numResourceTypes; k++) {
-				cout << finalProcesses[k][j] << " "; 
-			}cout << endl;
-		}cout << endl;
-		
+		//Go through the current processes in order
+		//Check if they can meet the requirements if they use the available resources
+			//If yes, hold them, and then terminate the processes by releasing all of its resources
+		//Repeat
+	
 	}
 	return 0;
 }
